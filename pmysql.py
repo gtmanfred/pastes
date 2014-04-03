388437fa900498c7da147c9534e922d833ff3e1b
Daniel Wallace <danielwallace@gtmanfred.com>
2014-04-02 19:02:07 -0500

    store db in mysql class

--- pmysql.py
+++ pmysql.py
@@ -2,39 +2,45 @@ import _mysql
 import ConfigParser
 import os.path
 
-def _get_conn():
-    config = ConfigParser.ConfigParser()
-    config.read(os.path.expanduser('~/.my.cnf'))
-    mysqluser = config.get('client', 'user')
-    mysqlpass = config.get('client', 'pass')
-    try:
-        mysqlhost = config.get('client', 'host')
-    except:
-        mysqlhost = 'localhost'
+class PasteConn(object):
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read(os.path.expanduser('~/.my.cnf'))
+        self.mysqluser = config.get('client', 'user')
+        self.mysqlpass = config.get('client', 'pass')
+        try:
+            self.mysqlhost = config.get('client', 'host')
+        except:
+            self.mysqlhost = 'localhost'
 
-    try:
-        mysqldb = config.get('client', 'database')
-    except:
-        mysqldb = 'pastes'
+        try:
+            self.mysqldb = config.get('client', 'database')
+        except:
+            self.mysqldb = 'pastes'
 
-    db = _mysql.connect(host=mysqlhost, user=mysqluser, passwd=mysqlpass, db=mysqldb)
-
-    return db
+        self.db = _mysql.connect(
+            host=self.mysqlhost,
+            user=self.mysqluser,
+            passwd=self.mysqlpass,
+            db=self.mysqldb
+        )
 
 def lookup(pasteid):
-    _db = _get_conn()
+    db = PasteConn()
+    _db = db.db
 
     try:
         _db.query((
-            'select paste from {0}.pastes where id={1};'
-        ).format(mysqldb, pasteid))
+            "select paste from {0}.pastes where id='{1}';"
+        ).format(db.mysqldb, pasteid))
         return _db.store_result()
     except:
         return False
 
 def post_paste(pasteid, text):
-    _db = _get_conn()
+    db = PasteConn()
+    _db = db.db
     entry = (
         "Insert into {0}.pastes (id, paste) values ('{1}', '{2}');"
-    ).format(mysqldb, pasteid, text)
+    ).format(db.mysqldb, pasteid, text)
     _db.query(entry)
